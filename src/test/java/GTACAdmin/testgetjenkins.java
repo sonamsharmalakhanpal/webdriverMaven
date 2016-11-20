47281f0bc5ca45eab30a9ea81918706514f4128f
Kris Brown <Kris Brown>
2016-11-20 13:59:46 +0530

    my third commit

---
+++ src/test/java/GTACAdmin/testgetjenkins.java
@@ -0,0 +1,24 @@
+package GTACAdmin;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class testgetjenkins {
+	
+	@Test
+	public void gmailtest(){
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\meetg\\Downloads\\chromedriver.exe");
+		WebDriver  driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.navigate().to("https://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("sonam.sharma2407");
+		 
+	}
+
+}
